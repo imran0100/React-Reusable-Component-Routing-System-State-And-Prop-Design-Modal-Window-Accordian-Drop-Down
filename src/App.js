a598f02ea802ebffc1d34a4b698381e47366dcbf
Imran Ansari <i4imran322@gmail.com>
2023-01-05 21:06:47 +0530

    Accordion complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
-import ButtonPage from "./pages/ButtonPage";
+import Accordion from "./components/Accordion";
 
 export default function App() {
+  const items = [
+    {
+      id: "gfttf",
+      label: "Can I use React on a project",
+      content:
+        "You can use React any project you want.You can use React any project you want.",
+    },
+    {
+      id: "ugdaz",
+      label: "Can I use Javascript on a project",
+      content:
+        "You can use Javascript any project you want.You can use Javascript any project you want.",
+    },
+    {
+      id: "hghhjiii",
+      label: "Can I use CSS on a project",
+      content:
+        "You can use CSS any project you want.You can use CSS any project you want.",
+    },
+  ];
+
   return (
     <div>
-      <ButtonPage />
+      <Accordion items={items} />
     </div>
   );
 }
